8ec6bb50629243738ec0cab92fb3301844cd09a4
Edo Maldonado <eduardo.maldonado@acidlabs.com>
2026-03-08 18:13:36 -0300

    build: Add GitHub Actions workflow for Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/portafolio',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
